12e378d85b7dc99b7357cdb9ee1523a1d611d18a
SimoneCalzari <simone.calzari@outlook.it>
2024-01-11 14:20:30 +0100

    feat: scaffolding

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP Dischi JSON</title>
+  <!-- MY STYLE -->
+  <link rel="stylesheet" href="src/style.css">
+  <!-- /MY STYLE -->
+</head>
+
+<body>
+  <div id="app">
+  </div>
+  <!-- VUE -->
+  <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+  <!-- /VUE -->
+  <!-- AXIOS -->
+  <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+  <!-- /AXIOS -->
+  <!-- MY JS -->
+  <script src="src/main.js"></script>
+  <!-- /MY JS -->
+</body>
+
+</html>
